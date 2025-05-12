ca4e5e48fe00a9e1d19ec8e79278295c471f4193
hana0627 <hanana9506@naver.com>
2025-05-12 19:19:51 +0900

    #1 implements UserControllerTest

--- cheers_up/src/main/java/com/hana/cheers_up/application/user/controller/UserAccountController.java
+++ cheers_up/src/main/java/com/hana/cheers_up/application/user/controller/UserAccountController.java
@@ -1,27 +1,15 @@
 package com.hana.cheers_up.application.user.controller;
 
-import com.hana.cheers_up.application.user.dto.UserAccountDto;
-import com.hana.cheers_up.application.user.service.UserAccountService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 @Controller
 public class UserAccountController {
 
-    private final UserAccountService userAccountService;
-
-    @PostMapping("/users/add")
-    public String addUser(UserAccountDto dto) {
-        log.info("[UserAccountController addUser] ");
-        userAccountService.saveUser(dto);
-        return "/cheers/search";
-    }
+//    private final UserAccountService userAccountService;
 
     @RequestMapping("/users/login")
     public String login() {
@@ -30,3 +18,12 @@ public class UserAccountController {
     }
 
 }
+
+// unused API
+//    @PostMapping("/users/add")
+//    public String addUser(@RequestBody UserAccountDto dto) {
+//        log.info("[UserAccountController addUser] ");
+//        System.out.println("여기온다!!!!!!!!!");
+//        userAccountService.saveUser(dto);
+//        return "/cheers/search";
+//    }
---
+++ cheers_up/src/test/java/com/hana/cheers_up/application/user/controller/UserAccountControllerTest.java
@@ -0,0 +1,32 @@
+package com.hana.cheers_up.application.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(UserAccountController.class)
+class UserAccountControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @WithMockUser
+    void 로그인_api호출시_검색페이지로_이동한다() throws Exception {
+        //given
+        // nothing
+
+        //when & then
+        mvc.perform(get("/users/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/cheers/search"));
+
+
+    }
+}
