89dcf65b8380d1525fdc7efa8b2d6b3cc38df1cf
hana0627 <hanana9506@naver.com>
2025-05-14 15:10:40 +0900

    ## Temporarily commented out code for testing

--- cheers_up/src/main/java/com/hana/cheers_up/application/pub/domain/Pub.java
+++ cheers_up/src/main/java/com/hana/cheers_up/application/pub/domain/Pub.java
@@ -1,26 +1,27 @@
-package com.hana.cheers_up.application.pub.domain;
-
-import lombok.Getter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Getter
-@Entity
-public class Pub {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String pubName;
-    private String pubAddress;
-    private double latitude;
-    private double longitude;
-    private String categoryName;
-
-    protected Pub pub(){
-        return new Pub();
-    }
-}
+//package com.hana.cheers_up.application.pub.domain;
+//
+//import lombok.Getter;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Getter
+//@Entity
+//public class Pub {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String pubName;
+//    private String pubAddress;
+//    private double latitude;
+//    private double longitude;
+//    private String categoryName;
+//
+//    protected Pub pub(){
+//        return new Pub();
+//    }
+//}
+// TODO 나중에 추가기능 개발할 때 활성화 하는것으로 함
--- cheers_up/src/main/java/com/hana/cheers_up/application/pub/repository/PubRepository.java
+++ cheers_up/src/main/java/com/hana/cheers_up/application/pub/repository/PubRepository.java
@@ -1,7 +1,8 @@
-package com.hana.cheers_up.application.pub.repository;
-
-import com.hana.cheers_up.application.pub.domain.Pub;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PubRepository extends JpaRepository<Pub, Long> {
-}
+//package com.hana.cheers_up.application.pub.repository;
+//
+//import com.hana.cheers_up.application.pub.domain.Pub;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface PubRepository extends JpaRepository<Pub, Long> {
+//}
+// TODO 나중에 추가기능 개발할 때 활성화 하는것으로 함
