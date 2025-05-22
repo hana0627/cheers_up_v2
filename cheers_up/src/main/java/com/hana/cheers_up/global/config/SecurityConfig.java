6a8139feb2153abb273a00cd421b6556b4e3aa81
hana0627 <hanana9506@naver.com>
2025-05-22 17:26:15 +0900

    #5 add CorsConfig(not implements test code)

--- cheers_up/src/main/java/com/hana/cheers_up/global/config/SecurityConfig.java
+++ cheers_up/src/main/java/com/hana/cheers_up/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,6 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .mvcMatchers(HttpMethod.GET,
